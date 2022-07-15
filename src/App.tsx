90a4883d039648a7afe61af51cbda4c819b9b168
Yorest <39180460+Yorest@users.noreply.github.com>
2022-07-15 11:37:19 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      React⚛️ + Vite⚡ + Replit🌀
+      <h1>hola</h1>
+    </main>
+  )
+}
